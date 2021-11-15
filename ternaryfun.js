72d8e0463f8552173f337e95e18a6b74b66601d9
lethidan <ltdan.sun.999@gmail.com>
2021-11-15 16:28:40 +0700

    uploaded

---
+++ ternaryfun.js
@@ -0,0 +1,8 @@
+var average=9;
+var rank=average>= 9 ?"Xuất sắc" :
+        average>=8 ?"Giỏi" :
+        average>=7 ?"Khá" :
+        average>=6 ?"Trung bình khá" :
+        average>=5 ?"Trung bình" : "Yếu";
+console.log(rank);        
+        
